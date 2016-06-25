f090bdd6f451396d2e2acd92b8de208e21e1ee2b
Korotkikh Sergei <software@reg51.ru>
2016-06-25 20:26:15 +0300

    Define all commands
    Define all commands of protocol

--- code/anviz-protocol/anviz-protocol.ino
+++ code/anviz-protocol/anviz-protocol.ino
@@ -1,3 +1,105 @@
+/* Arduino impl of Anviz protocol
+ * Date: 2016-06-25
+ * Author: Korotkikh Sergei
+ * Location: Russia, St.Petersburg
+ * Contacts: reg51ru@gmail.com
+ */
+
+// DEVICE STATE ANSWERS
+#define ACK_SUCCESS 0x00                    // operation successful
+#define ACK_FAIL 0x01                       // operation failed
+#define ACK_FULL 0x04                       // user full
+#define ACK_EMPTY 0x05                      // user empty
+#define ACK_NO_USER 0x06                    // user not exist
+#define ACK_TIME_OUT 0x08                   //capture timeout
+#define ACK_USER_OCCUPIED 0x0A              //user already exists
+#define ACK_FINGER_OCCUPIED 0x0B            //fingerprint already exists
+
+// COMMANDS
+#define CMD_P761_UPD_FIRMWARE 0x10          //only 761 platform. Update firmware, photo, voice
+#define CMD_P761_FILE_OPER 0x12             //only 761 platform. directory file operation
+#define CMD_P761_GET_LOG_RECOED 0x13        //only 761 platform. Download log record
+#define CMD_ALL_GET_GPRS_PARAMS 0x16        //Get GPRS parameter
+#define CMD_ALL_SET_GPRS_PARAMS 0x17        //Set GPRS parameter
+#define CMD_ALL_GET_OPT_LANG 0x18           //Read optional language combination
+#define CMD_ALL_SET_OPT_LANG 0x19           //Set optional language combination
+#define CMD_ALL_GET_DAYLIGHT_PRM 0x1A       //Read daylight saving parameter
+#define CMD_ALL_SET_DAYLIGHT_PRM 0x1B       //Set daylight saving time parameter
+#define CMD_T5_GET_ADMIN_CARD 0x1C          //only T5. Read admin card number/admin password
+#define CMD_T5_SET_ADMIN_CARD 0x1D          //only T5. Read optional language combination
+#define CMD_OA3000_GET_ASWITCH 0x20         //only for OA3000 & OA1000. Get T&A state auto switch setting
+#define CMD_OA3000_SET_ASWITCH 0x21         //only for OA3000 & OA1000. Set T&A state auto switch setting
+#define CMD_P761_GET_STAFF_EXT 0x22         //only 761 platform. Download staff information (extended)
+#define CMD_P761_SET_STAFF_EXT 0x23         //only 761 platform. Upload staff information (extend)
+#define CMD_ALL_GET_DEV_SN 0x24             //Get device serial number
+#define CMD_ALL_SET_DEV_SN 0x25             //Modify device serial number
+#define CMD_OA3000_GET_MSG_ID 0x26          //only for OA3000. Get specified index message
+#define CMD_OA3000_ADD_MSG 0x27             //only for OA3000. Add new message
+#define CMD_OA3000_GET_MSG_LIST 0x28        //only for OA3000. Read message head of assigned section message
+#define CMD_OA3000_DEL_MSG 0x29             //only for OA3000. Delete appointed index message
+#define CMD_OA3000_GET_PHOTO_CNT 0x2A       //OA1000/OA3000/761platform. Get photo amount
+#define CMD_OA3000_GET_PHOTO_HEAD 0x2B      //OA1000/OA3000/761platform. Get photo head information
+#define CMD_OA3000_GET_PHOTO 0x2C           //OA1000/OA3000/761platform. Read specified photo file
+#define CMD_OA3000_DEL_PHOTO 0x2D           //OA1000/OA3000/761platform. Delete specified photo
+#define CMD_V30_GET_SPEC_STATE 0x2F         //VF30/VP30/T60+use only. Get special state
+#define CMD_ALL_GET_INFO 0x30               //Get the information of T&A device 1
+#define CMD_ALL_SET_INFO 0x31               //Set the configure information of T&A 1
+#define CMD_ALL_GET_INFO2 0x32              //Get the information of T&A device 2
+#define CMD_ALL_SET_INFO2 0x33              //Set the configure information of T&A 2
+#define CMD_ALL_GET_TIME 0x38               //Get the date and time of T&A
+#define CMD_ALL_SET_TIME 0x39               //Set the date and time of T&A
+#define CMD_ALL_GET_TCPPARAMS 0x3A          //Get TCP/IP parameters
+#define CMD_ALL_SET_TCPPARAMS 0x3B          //Set TCP/IP parameters
+#define CMD_ALL_GET_RECORD_INFO 0x3C        //Get record information
+#define CMD_ALL_CLEAR_ADM_FLAG 0x3D         //Clear administrator flag
+#define CMD_ALL_GET_ENROLL_DATE 0x3E        //Read employees enrollment timestamp
+#define CMD_ALL_SET_ENROLL_DATE 0x3F        //Set time stamp
+#define CMD_ALL_GET_RECORDS 0x40            //Download T&A records
+#define CMD_ALL_SET_RECORDS 0x41            //Upload T&A records
+#define CMD_ALL_GET_STAFF 0x42              //Download staff info
+#define CMD_ALL_SET_STAFF 0x43              //Upload staff info
+#define CMD_ALL_GET_FP_TPL 0x44             //Download FP Template
+#define CMD_ALL_SET_FP_TPL 0x45             //Upload FP Template
+#define CMD_ALL_GET_SN 0x46                 //Get device S/N
+#define CMD_ALL_SET_SN 0x47                 //Modify device S/N
+#define CMD_ALL_GET_TYPE 0x48               //Get device type code
+#define CMD_ALL_SET_TYPE 0x49               //Modify device type code
+#define CMD_ALL_GET_FACTORY 0x4A            //Get the factory info code
+#define CMD_ALL_SET_FACTORY 0x4B            //Modify the factory info code
+#define CMD_ALL_DEL_USERDATA 0x4C           //Delete the designated user data
+#define CMD_ALL_INIT_USERAREA 0x4D          //Initialize the user area
+#define CMD_ALL_CLR_RECORDS 0x4E            //Clear up Records /Clear new records sign
+#define CMD_ALL_INIT_SYSTEM 0x4F            //Initialize System
+#define CMD_ALL_GET_TIMEZONE 0x50           //Get the time zone info
+#define CMD_ALL_SET_TIMEZONE 0x51           //Set time zone info
+#define CMD_ALL_GET_GROUPINFO 0x52          //Get the group info
+#define CMD_ALL_SET_GROUPINFO 0x53          //Set the group info
+#define CMD_ALL_GET_BELL_TASKS 0x54         //Get the scheduled bell info
+#define CMD_ALL_SET_BELL_TASKS 0x55         //Set ring info
+#define CMD_ALL_GET_SPEC_MSG 0x56           //Retrieve specified short message
+#define CMD_ALL_ADD_SPEC_MSG 0x57           //Add short message
+#define CMD_ALL_READ_ALL_SPEC_MSG 0x58      //Read all info head of all short message
+#define CMD_ALL_DEL_SPEC_MSG 0x59           //Delete specified index short messageage
+#define CMD_ALL_GET_MSG_STATE 0x5A          //Get T&A State message
+#define CMD_ALL_SET_PARAM_TBL 0x5B          //Set T&A State parameter table
+#define CMD_ALL_ENROLL_USER_FP 0x5C         //Enroll user FP online
+#define CMD_ALL_GET_CAPACITY 0x5D           //Get device capacity parameter
+#define CMD_ALL_OPEN_DOOR 0x5E              //Output signal to open lock without verifying user
+#define CMD_ALL_SENT_RECORD 0x5F            //Sent T&A record in real time
+#define CMD_ALL_GET_CUST_TBL 0x70           //Get customized T&A state table
+#define CMD_ALL_SET_CUST_TBL 0x71           //Set attendance state table
+#define CMD_ALL_GET_STAFF_DATA 0x72         //Download employees data
+#define CMD_ALL_SET_STAFF_DATA 0x73         //Upload staff information(extended)
+#define CMD_ALL_GET_COM_DEVID 0x74          //Get communication device ID
+#define CMD_ALL_SET_COM_DEVID 0x75          //Modify communication device ID
+#define CMD_ALL_GET_RANDOM 0x76             //Read random number
+#define CMD_ALL_ENC_DEVINFO 0x77            //Encrypt device type and language with random number
+#define CMD_ALL_RETR_FEATURE_VAL 0x78       //Receive feature value /card ID to execute following operation
+#define CMD_ALL_GET_DEVICE_INFO_EXT 0x7A    //Get device extended information code
+#define CMD_ALL_SET_DEVICE_INFO_EXT 0x7B    //Modify device extend message code
+#define CMD_T5S_INQUIRE_CARD_INFO 0x7E      //T5S only. inquire information of card number
+#define CMD_C5_EMAIL_SETTINGS 0x7F          //C5 only. Sending Email
+
 // crc16 bits
 unsigned short _crc_table[] = {
     0x0000,0x1189,0x2312,0x329b,0x4624,0x57ad,0x6536,0x74bf,0x8c48,0x9dc1,
@@ -69,13 +171,30 @@ void sendCommand(byte command, unsigned long device_id, byte data[], unsigned sh
     Serial.println("");    
     }
 
+/* == Get the information of T&A device 1 == 
+ * Get the firmware version, communication password, sleep time, volume, language, date
+ * and time format, attendance state, language setting flag, command version.
+ *
+ * params @device_id - when is "0" then all devices connected will response to this command
+ */
+void getDeviceInfo(unsigned long device_id){
+  sendCommand(CMD_ALL_GET_INFO,device_id,{},0);
+  }
+
+/* == Output signal to open lock without verifying user 
+ * Force T&A device output signal to open door
+ *
+ * params @device_id - when is "0" then all devices connected will response to this command
+ */
+void openDoor(unsigned long device_id){
+  sendCommand(CMD_ALL_OPEN_DOOR,device_id,{},0);
+  }
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  sendCommand(0x5E,0,{},0);
+  openDoor(0);
   }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
 }
